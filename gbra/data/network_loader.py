b770acc0c40f552212b669a023f60f187edf3ecd
Alan <alanf94@stanford.edu>
2018-11-02 14:53:42 -0700

    Adds erdos renyi bipartite graph generation to network loader class -- updates sample experiment to exercise random erdos renyi graph

--- gbra/data/network_loader.py
+++ gbra/data/network_loader.py
@@ -3,9 +3,12 @@ Classes and routines for loading SNAP networks
 """
 
 import snap
+import random
 
 class NetworkLoader(object):
     """Loads bipartite entity-item network for our research project.
+
+    Can also load an Erdos-Renyi bipartite graph.
 
     Enforces the condition that entities have ODD ids and items have
     EVEN ids.
@@ -42,10 +45,41 @@ class NetworkLoader(object):
 
         return G
 
-
     def load_network(self, name):
         name = name.lower()
         if name == "tiny_test":
             return self._load_tiny_test_network()
         else:
             raise ValueError("Network name %s unknown.")
+
+    def get_erdos_renyi_bipartite_graph(self, num_entities, num_items, num_edges):
+        """
+        :param - num_entities: number of entities to include
+        :param - num_items: number of items to include
+        :param - num_edges: the number of edges desired.
+
+        return type: snap.PUNGraph
+        return: Erdos-Renyi graph bipartite graph with NUM_ENTITIES,
+                NUM_ITEMS, and NUM_EDGES chosen uniformly at random between
+                entities and items.
+        """
+
+        if num_edges > num_entities * num_items:
+            raise ValueError("More edges requested than possible.")
+
+        Graph = snap.TUNGraph.New()
+        for i in range(num_entities):
+            Graph.AddNode(2 * i + 1)
+
+        for i in range(num_items):
+            Graph.AddNode(2 * i)
+
+        edges_left = num_edges
+        while edges_left > 0:
+            entity_node_id = 2 * random.randint(0, num_entities - 1) + 1
+            item_node_id = 2 * random.randint(0, num_items - 1)
+            if not Graph.IsEdge(entity_node_id, item_node_id):
+                edges_left -= 1
+                Graph.AddEdge(entity_node_id, item_node_id)
+
+        return Graph
--- sample_exp.py
+++ sample_exp.py
@@ -3,11 +3,28 @@ from gbra.recommender.recommenders import RandomRecommender
 from gbra.data.network_loader import NetworkLoader
 
 
+# Tiny Test
 loader = NetworkLoader()
 tiny_network = loader.load_network("tiny_test")
 recommender = RandomRecommender(tiny_network)
 recs_for_3 = recommender.recommend(entity_id=3, number_of_items=3)
 recs_for_5 = recommender.recommend(entity_id=5, number_of_items=2)
 
-print("Recommendations for 3: %s" % str(recs_for_3))
-print("Recommendations for 5: %s" % str(recs_for_5))
+print("Tiny test")
+print("Random recommendations for 3: %s" % str(recs_for_3))
+print("Random recommendations for 5: %s" % str(recs_for_5))
+print("---")
+
+# Erdos-renyi bipartite graph
+loader = NetworkLoader()
+er_network = loader.get_erdos_renyi_bipartite_graph(num_entities=20, num_items=50, num_edges=60)
+recommender = RandomRecommender(er_network)
+recs_for_15 = recommender.recommend(entity_id=15, number_of_items=3)
+recs_for_21 = recommender.recommend(entity_id=21, number_of_items=5)
+
+print("Erdos Renyi:")
+print("Random recommendations for 15: %s" % str(recs_for_15))
+print("Random recommendations for 21: %s" % str(recs_for_21))
+
+assert(er_network.GetNodes() == 20 + 50)
+assert(er_network.GetEdges() == 60)
