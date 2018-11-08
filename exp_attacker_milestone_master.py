1ed8b9cd67ac0259b62d3776f249b062a1685985
Divyahans Gupta <divyahansg@gmail.com>
2018-11-08 00:46:52 -0800

    Raw CMD

--- exp_attacker_milestone_master.py
+++ exp_attacker_milestone_master.py
@@ -5,8 +5,5 @@ for attacker_name in ["Random", "Average", "Neighbor"]:
         for percent_fake_entities in [0.005, 0.01, 0.03, 0.05]:
             for fake_reviews in [1, 2, 3, 4, 5, 6, 8, 10]:
                 filename = '-'.join([attacker_name, graph_name, str(percent_fake_entities), str(fake_reviews)])
-                with open(filename, "w") as out:
-                    subprocess.Popen(
-                        ["python", "exp_attacker_milestone.py", graph_name, str(percent_fake_entities), str(fake_reviews)],
-                        stdout=out, stderr=out
-                    )
+                cmd = ' '.join(["python", "exp_attacker_milestone.py", graph_name, str(percent_fake_entities), str(fake_reviews)])
+                os.system(cmd + " > " + filename)
