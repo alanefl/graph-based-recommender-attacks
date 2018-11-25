7c15f50ce00247c25a226cf71bc6cf9eada7306f
Alan <alanf94@stanford.edu>
2018-11-24 18:13:21 -0800

    Add parser script for milestone recommender evaluation outputs

---
+++ milestone_experiments/exp_milestone_results_parser.py
@@ -0,0 +1,37 @@
+"""Parses recommender evaluation results files.
+"""
+
+import os
+
+EXP_DIRECTORY = "milestone_rec_eval_results"
+
+experiment_sums = {}
+experiment_counts = {}
+
+for filename in os.listdir(EXP_DIRECTORY):
+    if filename.endswith(".milestone_recommender_eval"):
+        full_path = os.path.join(EXP_DIRECTORY, filename)
+        with open(full_path, "r") as results_file:
+            for line in results_file:
+                line = line.strip()
+                graph_pair, num_recs_pair, score_pair, recommender_pair, iter_pair = line.split(",")
+                graph_name = graph_pair.split(":")[1]
+                num_recs = int(num_recs_pair.split(":")[1])
+                score = float(score_pair.split(":")[1])
+                recommender = recommender_pair.split(":")[1]
+                iteration = int(iter_pair.split(":")[1])
+
+                experiment_id = (graph_name, num_recs, recommender)
+
+                if experiment_id not in experiment_sums:
+                    experiment_sums[experiment_id] = 0
+                if experiment_id not in experiment_counts:
+                    experiment_counts[experiment_id] = 0
+
+                experiment_sums[experiment_id] += score
+                experiment_counts[experiment_id] += 1
+
+for k, v in experiment_sums.items():
+    print("Experiment: %s, average recommender hit ratio: %f" % (
+        str(k), v / float(experiment_counts[k])
+    ))
