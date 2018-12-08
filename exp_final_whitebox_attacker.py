dfec863ca409c25d731a1c10281f9a23d45f2273
Leo Mehr <leo.mehr@gmail.com>
2018-12-08 15:24:48 -0800

    blackbox attacker experiment framework

--- exp_final_whitebox_attacker.py
+++ exp_final_whitebox_attacker.py
@@ -31,13 +31,34 @@ attackers = {
     'HillClimbingAttacker': HillClimbingAttacker,
     'RandomAttacker': RandomAttacker,
     'AverageAttacker': AverageAttacker,
-    'NeighborAttacker': NeighborAttacker
+    'NeighborAttacker': NeighborAttacker,
+    'BlackBoxRWRAttacker': BlackBoxRWRAttacker,
+    'BlackBoxDeepRWRAttacker': BlackBoxDeepRWRAttacker
 }
+
+BLACK_BOX_RWR_NUM_SCOUT_ITEMS = 100
 
 def get_attacker(network, recommender, target_item):
     attacker_klass = attackers[ATTACKER_NAME]
     num_fake_entities = int(PERCENT_FAKE_ENTITIES * network.num_entities)
-    return attacker_klass(recommender, target_item, num_fake_entities, NUM_FAKE_REVIEWS)
+
+    kwargs = dict(
+        _recommender=recommender,
+        _target_item=target_item,
+        _num_fake_entities=num_fake_entities,
+        _num_fake_ratings=NUM_FAKE_REVIEWS
+    )
+    if ATTACKER_NAME == 'BlackBoxRWRAttacker':
+        kwargs.update(dict(
+            _num_items_to_scout=BLACK_BOX_RWR_NUM_SCOUT_ITEMS,
+            _num_recs=RECOMMENDATIONS
+        ))
+    elif ATTACKER_NAME = 'BlackBoxDeepRWRAttacker':
+        kwargs.update(dict(
+            _num_items_to_scout=BLACK_BOX_RWR_NUM_SCOUT_ITEMS,
+        ))
+
+    return attacker_klass(**kwargs)
 
 def evaluate_attacker(target_item):
     network = Movielens100kLoader().load()
@@ -45,25 +66,25 @@ def evaluate_attacker(target_item):
     recommender = PixieRandomWalkRecommender(G=network, **PIXIE_PARAMS)
     attacker = get_attacker(network, recommender, target_item)
 
-    # before = recommender.calculate_hit_ratio(target_item, RECOMMENDATIONS, verbose=False)
+    before = recommender.calculate_hit_ratio(target_item, RECOMMENDATIONS, verbose=False)
     try:
         attacker.attack()
     except:
         traceback.print_exc()
     after = recommender.calculate_hit_ratio(target_item, RECOMMENDATIONS, verbose=False)
-    return (0, after)
+    return (before, after)
 
 results = []
 network = Movielens100kLoader().load()
-target_items = network.get_random_items(ITERATIONS)
-print target_items
+# target_items = network.get_random_items(ITERATIONS)
+# print target_items
+target_items = [2352, 380, 1722, 2514, 2384]
+
 for i in range(ITERATIONS):
+    print i
     (before, after) = evaluate_attacker(target_items[i])
-    print (before, after)
     results.append((before, after))
 print results
 
 arr = np.array([a[1] for a in results])
-print "mean: %f, median %f" % (np.mean(arr), np.median(arr))
-
-
+print "mean: {}, median {}, std dev {}".format(np.mean(arr), np.median(arr), np.std(arr))
